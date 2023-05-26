4246c3419b5f2e87788cf56986ee31e91dd1e056
Andy <dta89uct@gmail.com>
2023-05-27 00:51:29 +0700

    map sql native query result into DTO in spring jpa repository

--- src/main/java/com/andy/attire/entity/ProductImageEntity.java
+++ src/main/java/com/andy/attire/entity/ProductImageEntity.java
@@ -1,5 +1,6 @@
 package com.andy.attire.entity;
 
+import com.andy.attire.entity.resultset.ResultSet;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -7,6 +8,18 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "product_image")
+@NamedNativeQuery(
+        name = "find_data",
+        query = "SELECT c.id,c.url as imgUrl FROM product_image c where c.id=:id",
+        resultSetMapping = "imageMapToResultSet"
+)
+@SqlResultSetMapping(name="imageMapToResultSet",classes = @ConstructorResult(
+        targetClass = ResultSet.class,
+        columns = {
+                @ColumnResult(name="id",type = Long.class),
+                @ColumnResult(name = "imgUrl")
+        }
+))
 public class ProductImageEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/andy/attire/repository/ProductImageRepository.java
+++ src/main/java/com/andy/attire/repository/ProductImageRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface ProductImageRepository extends CrudRepository<ProductImageEntity,Long> {
-    @Query("SELECT new com.andy.attire.entity.resultset.ResultSet( c.id,c.product.brand) FROM ProductImageEntity c where c.id=:id")
+    @Query(name = "find_data",nativeQuery = true)
     ResultSet countData(@Param("id") Long id);
 }
