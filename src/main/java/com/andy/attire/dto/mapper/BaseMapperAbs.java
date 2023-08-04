0b3bcb15c81437c345f237ee76e977d72a884852
Andy <dta89uct@gmail.com>
2023-08-04 15:37:33 +0700

    fix some bugs

--- src/main/java/com/andy/attire/dto/mapper/BaseMapperAbs.java
+++ src/main/java/com/andy/attire/dto/mapper/BaseMapperAbs.java
@@ -1,7 +1,6 @@
 package com.andy.attire.dto.mapper;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 
 public abstract class BaseMapperAbs {
     protected static ModelMapper getModelMapper(){
--- src/main/java/com/andy/attire/repository/ProductImageRepository.java
+++ src/main/java/com/andy/attire/repository/ProductImageRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface ProductImageRepository extends CrudRepository<ProductImageEntity,Long> {
     @Query(name = "find_data",nativeQuery = true)
